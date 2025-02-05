fa1b6abeb76b95eb41158267a80a6ff43af87748
José Nilson <jns.violinista@gmail.com>
2025-02-04 22:47:25 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#carousel-imagens").slick({
+    autoplay: true,
+  });
+
+  $("#telefone").mask("(00) 00000-0000");
+
+  $("form").validate({
+    rules: {
+      nome: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      telefone: {
+        required: true,
+      },
+    },
+  });
+});
